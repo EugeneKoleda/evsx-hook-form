e3c29def7e93c0637e608ab232b7883f8d9bc75a
Yauheni Kaliada <Yauheni.Kaliada@itechart-group.com>
2020-12-09 17:05:05 +0300

    Add values, onSubmit and onChange

--- src/hooks/useForm.tsx
+++ src/hooks/useForm.tsx
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, { SyntheticEvent, useMemo, useState } from 'react';
 
-let useForm = () => {
-    console.log('Hello');
+type ValueType = string | number | boolean | [] | null; 
 
-    return (<h1>Test!</h1>);
+type ValuesType = {
+    [key: string]: ValueType;
+};
+
+type StateMapType = {
+    [key: string]: [ValueType, React.Dispatch<React.SetStateAction<ValueType>>]
+};
+
+interface IProps {
+    onSubmit: (values: ValuesType) => void;
+    initialValues: ValuesType;
+};
+
+interface IForm {
+    values: null | ValuesType;
+    onSubmit: (e: SyntheticEvent<HTMLFormElement>) => void;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+let useForm = (props: IProps) => {
+    let { initialValues, onSubmit } = props;
+    let form: IForm = {
+        values: null,
+        onSubmit: () => {},
+        onChange: () => {},
+    };
+
+    let stateMap = useMemo(() => 
+        Object.keys(initialValues).reduce((result: StateMapType, key: string): StateMapType => {
+            let initialValue = initialValues[key];
+
+            result[key] = useState(initialValue);
+
+            return result;
+        }, {}), 
+        [initialValues]
+    ); 
+
+    form.values = useMemo(() => 
+        Object.keys(stateMap).reduce((result: ValuesType, key: string): ValuesType => {
+            let [value] = stateMap[key];
+
+            result[key] = value;
+
+            return result;
+        }, {}), 
+        [stateMap]
+    );
+
+    form.onSubmit = (e: SyntheticEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if(form.values) {
+            onSubmit(form.values);
+        }
+    };
+
+    form.onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        let value = e.target.value;
+        let name = e.target.name;
+
+        let [, setValue] = stateMap[name];
+
+        setValue(value);
+    };
+
+    return form;
 }
 
 export default useForm;
