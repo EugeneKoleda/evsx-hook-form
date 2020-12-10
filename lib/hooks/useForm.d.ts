8d8646e196065d7874e47c809d71882ccacd19ae
Yauheni Kaliada <Yauheni.Kaliada@itechart-group.com>
2020-12-10 17:02:25 +0300

    Extend interface

--- lib/hooks/useForm.d.ts
+++ lib/hooks/useForm.d.ts
@@ -1,4 +1,17 @@
-/// <reference types="react" />
-declare let useForm: () => JSX.Element;
+import React, { SyntheticEvent } from 'react';
+declare type ValueType = string | number | boolean | [] | null;
+declare type ValuesType = {
+    [key: string]: ValueType;
+};
+interface IProps {
+    onSubmit: (values: ValuesType) => void;
+    initialValues: ValuesType;
+}
+interface IForm {
+    values: null | ValuesType;
+    onSubmit: (e: SyntheticEvent<HTMLFormElement>) => void;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+declare let useForm: (props: IProps) => IForm;
 export default useForm;
 //# sourceMappingURL=useForm.d.ts.map
--- src/hooks/useForm.tsx
+++ src/hooks/useForm.tsx
@@ -10,6 +10,20 @@ type StateMapType = {
     [key: string]: [ValueType, React.Dispatch<React.SetStateAction<ValueType>>]
 };
 
+interface IFieldProps {
+    name: string;
+    type: string;
+    Component?: () => JSX.Element;
+    id?: string;
+    className?: string;
+};
+
+interface IInternalProps extends IFieldProps {
+    checked?: ValueType;
+    value?: ValueType;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
 interface IProps {
     onSubmit: (values: ValuesType) => void;
     initialValues: ValuesType;
@@ -19,7 +33,11 @@ interface IForm {
     values: null | ValuesType;
     onSubmit: (e: SyntheticEvent<HTMLFormElement>) => void;
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    setFieldValue: (name: string, value: ValueType) => void;
+    useField: (fieldProps: IFieldProps) => JSX.Element | ((props: any) => JSX.Element) | void;
 };
+
+
 
 let useForm = (props: IProps) => {
     let { initialValues, onSubmit } = props;
@@ -27,6 +45,8 @@ let useForm = (props: IProps) => {
         values: null,
         onSubmit: () => {},
         onChange: () => {},
+        setFieldValue: () => {},
+        useField: () => {},
     };
 
     let stateMap = useMemo(() => 
@@ -59,13 +79,37 @@ let useForm = (props: IProps) => {
         }
     };
 
-    form.onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        let value = e.target.value;
-        let name = e.target.name;
-
+    form.setFieldValue = (name: string, value: ValueType) => {
         let [, setValue] = stateMap[name];
 
         setValue(value);
+    };
+
+    form.onChange = (e: React.ChangeEvent<HTMLInputElement>) => { 
+        let name = e.target.name;
+        let isCheckbox = e.target.type === 'checkbox';
+        let value = isCheckbox ? e.target.checked : e.target.value;
+
+        form.setFieldValue(name, value);
+    };
+
+    form.useField = (fieldProps: IFieldProps) => {
+        let  { Component, ...commonProps } = fieldProps;
+
+        let internalProps: IInternalProps = { ...commonProps, onChange: form.onChange };
+        let [fieldValue] = stateMap[internalProps.name];
+
+        if (internalProps.type === 'checkbox') {
+            internalProps.checked = fieldValue;
+        } else {
+            internalProps.value = fieldValue;
+        }
+
+        if (Component) {
+            return <Component />;
+        }
+
+        return (props: any): JSX.Element => (<input {...internalProps} {...props} />);
     };
 
     return form;
